167cba4e5c28b298951a15f159c0e1ae6a883ea0
Szymekc <szymekc@interia.eu>
2017-02-12 21:46:23 +0100

    123 123

---
+++ division.py
@@ -0,0 +1,16 @@
+def division(poly,d):
+    poly = [2,1,0,1]
+    d = [1,1]
+    #try:
+  #      result = poly/d
+   # except ZeroDivisionError:
+  #      print("divide by zero")
+    q = []
+    r = poly
+    while r != [] and poly[-1] >= r[-1]:
+     #   t =
+     #          q.setdefault(key, []).extend(value)
+  #      for key, value in t.iteritems():
+  #          r.setdefault(key,[]).extend(value)
+  #      r =
+    return (q, r)
