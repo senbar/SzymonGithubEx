274b2dc034c7a66bc8fb34311a4bf8e5fb08fbcc
Szymekc <szymekc@interia.eu>
2017-02-14 23:28:07 +0100

    division wciaz nie dziala

--- division.py
+++ division.py
@@ -1,16 +1,26 @@
-def division(poly,d):
-    poly = [2,1,0,1]
-    d = [1,1]
-    #try:
-  #      result = poly/d
-   # except ZeroDivisionError:
-  #      print("divide by zero")
-    q = []
-    r = poly
-    while r != [] and poly[-1] >= r[-1]:
-     #   t =
-     #          q.setdefault(key, []).extend(value)
-  #      for key, value in t.iteritems():
-  #          r.setdefault(key,[]).extend(value)
-  #      r =
-    return (q, r)
+#NIE DZIALA XD
+from polyUnit import *
+from translate import *
+poly = Translate(input("Input polynomial: "))
+div = Translate(input("Input divisor: "))
+def Division(poly,div):
+    quo = polyUnit()
+    t = polyUnit()
+    rem = polyUnit()
+    rem.exponent = poly.exponent
+    rem.factor = poly.factor
+    i = 0
+    while rem != 0 and rem[-1].exponent >= div[-1].exponent:
+        i += 1
+        t[0].exponent = rem[-1].exponent - div[-1].exponent
+        t[0].factor = rem[-1].factor / div[-1].factor
+        quo[-i].exponent = t[0].exponent
+        quo[-i].factor = t[0].factor
+        rem[-1].exponent = rem.exponent - i
+        rem[-i].factor = rem.factor - t[0].factor * div[-i].factor
+    return(quo,rem)
+    print(quo[-1].factor)
+    print(quo[-1].exponent)
+
+    #print(t.exponent)
+    #print(poly[1].factor)
